548dd275b8eda3d4adf9550f3783e65569759cdb
Caroline Rezende <caroline6254@hotmail.com>
2021-11-23 16:35:16 -0300

    rent controller ok

--- app/controllers/rents_controller.rb
+++ app/controllers/rents_controller.rb
@@ -1,2 +1,36 @@
 class RentsController < ApplicationController
+  before_action :set_rent, only: [:show, :destroy]
+
+  def show
+    @rent = Rent.find(params[:id])
+  end
+
+  def new
+    @rent = Rent.new
+  end
+
+  def create
+    @rent = Rent.new(rent_params)
+    @rent.user = current_user
+    if @rent.save
+      redirect_to rent_path(@rent)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @rent.destroy
+     redirect_to rents_path
+  end
+
+   private
+
+  def rent_params
+    params.require(:rent).permit(:period_of_time)
+  end
+
+  def set_rent
+    @rent = Rent.find(params[:id])
+  end
 end
