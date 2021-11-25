1faeaa00e1ec1f36d5bd0538220996b57cf5507e
Caroline Rezende <caroline6254@hotmail.com>
2021-11-25 15:58:23 -0300

    NEW VIEW CREATED

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_24_185846) do
+ActiveRecord::Schema.define(version: 2021_11_25_182334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,10 +46,10 @@ ActiveRecord::Schema.define(version: 2021_11_24_185846) do
   create_table "games", force: :cascade do |t|
     t.string "name"
     t.string "category"
-    t.integer "price"
-    t.integer "number_of_players"
+    t.string "price"
+    t.string "number_of_players"
     t.text "description"
-    t.boolean "available"
+    t.boolean "available", default: true
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
