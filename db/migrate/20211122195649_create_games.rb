a74fde61b12b4e2ddcc27c85d401467156db5b47
Caroline Rezende <caroline6254@hotmail.com>
2021-11-25 17:06:46 -0300

    VIEW NEW 2

--- db/migrate/20211122195649_create_games.rb
+++ db/migrate/20211122195649_create_games.rb
@@ -3,7 +3,7 @@ class CreateGames < ActiveRecord::Migration[6.1]
     create_table :games do |t|
       t.string :name
       t.string :category
-      t.integer :price
+      t.float :price
       t.string :number_of_players
       t.text :description
       t.boolean :available, default: true
